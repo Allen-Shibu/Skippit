a7c867187f75a630f32a134659f86bbc42857419
Allen Shibu <allenshibukuriakose@gmail.com>
2025-12-05 22:33:57 +0530

    Updated Commit

--- index.js
+++ index.js
@@ -21,6 +21,30 @@ function switchTab(tabName) {
         scheduleBtn.setAttribute('id','start')
     }
 }
+let SaveBtn=document.getElementById("save-btn")
+let Subject = document.getElementById("subject-name");
+let ClassNum = document.getElementById("class-num");
+let TotalClass = document.getElementById("total-class");
+let EndDate = document.getElementById("end-date");
+
+function save() {
+    localStorage.setItem('Subject_Name', Subject.value)
+    localStorage.setItem("Class_Attended", ClassNum.value);
+    localStorage.setItem("Total_Class", TotalClass.value);
+    localStorage.setItem("End_Date", EndDate.value);    
+
+    if (Number(ClassNum.value) > Number(TotalClass.value)) {
+        alert("Total Classes cannot be less than attended classes")
+    }
+    console.log(Subject.value);
+    console.log(ClassNum.value);
+    console.log(TotalClass.value);
+    console.log(EndDate.value);
+}
+
+function add_subject() {
+    localStorage.getItem('Subject_Name')
+}
 
 function toggle() {
     let blur = document.getElementById('blur')
